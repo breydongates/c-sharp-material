4669f92522d52858d62aebfaeb19bf8cad7c9ef6
Matt Eland <matt.eland@gmail.com>
2020-09-16 12:54:21 -0400

    M1-3 Lecture Final

---
+++ module-1/03_Expressions/lecture-final/dotnet/Lecture/12_ReturnFizzIfThreeUsingTernary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    public partial class LectureExample
+    {
+
+        /*
+        12. Now write the above using the Ternary operator ?:
+        */
+        public string ReturnFizzIfThreeUsingTernary(int number)
+        {
+            return (number == 3) ? "Fizz" : ""; // Do not use this or we will frown
+
+            /*
+            if (number == 3)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return ""; // "" is an empty string
+            }
+            */
+        }
+    }
+}
---
+++ module-1/03_Expressions/lecture-final/dotnet/Lecture/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string message = null;
+            Console.WriteLine(message);
+
+            int someNumber = 0;
+            Console.WriteLine(someNumber);
+
+            Console.WriteLine("Message is above ^");
+
+            // TODO: Talk about casting
+
+            // TODO: Talk about double / decimal / float
+
+            double someDouble = 4.2;
+            decimal someDecimal = 4.2M;
+            decimal someOtherDecimal = (decimal)4.2;
+            float someFloat = 4.2F;
+
+            int someInt = (int)someDouble;
+            Console.WriteLine(someInt);
+
+            // how do you write out something like x=(53-2)/3
+            // If two lemon cupcakes were left at home, how many boxes 
+            // with 3 lemon cupcakes each were given away
+
+            int numCupcakes = 53;
+            int numLeftAtHome = 1;
+
+            const int cupcakesPerBox = 3; // Const is a variable that cannot change
+
+            int boxes = (numCupcakes - numLeftAtHome) / cupcakesPerBox; // 17
+
+            Console.WriteLine("We'll need " + boxes + " boxes");
+
+            if (boxes < 7)
+            {
+                Console.WriteLine("We'll need more boxes");
+            }
+            else if (boxes > 15)
+            {
+                Console.WriteLine("That's a lot of boxes!");
+                Console.WriteLine("Great googely moogely - the boxes!");
+            }
+            else
+            {
+                Console.WriteLine("We have plenty of boxes for this");
+            }
+        }
+    }
+}
