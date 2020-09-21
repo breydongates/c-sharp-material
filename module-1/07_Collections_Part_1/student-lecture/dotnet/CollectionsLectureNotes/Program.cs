cc29eb4db33458d607258c93ad2e391e356e83a3
Matt Eland <matt.eland@gmail.com>
2020-09-21 15:58:14 -0400

    M1-7 Student Exercise & Lecture

---
+++ module-1/07_Collections_Part_1/student-lecture/dotnet/CollectionsLectureNotes/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsLectureNotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // LIST<T>
+            //
+            // Lists allow us to hold collections of data. They are declared with a type of data that they hold
+            // only allowing items of that type to go inside of them.
+
+            // Creating lists of integers
+
+
+            // Creating lists of strings
+
+            /////////////////
+
+
+            //////////////////
+            // OBJECT EQUALITY
+            //////////////////
+
+            // Check reference equality
+
+            // Compare two variables pointed at the same list
+
+            /////////////////
+            // ADDING ITEMS
+            /////////////////
+
+            // Adding items one at a time to each list
+
+            // ADDING MULTIPLE ITEMS
+
+            // LIST INITIALIZER
+
+            //////////////////
+            // ACCESSING BY INDEX
+            //////////////////
+
+
+
+            ///////////////////
+            // ACCESSING WITH FOREACH
+            ///////////////////
+
+
+
+            ////////////////////
+            // ADDITIONAL LIST<T> METHODS
+            ////////////////////
+
+            // Contains
+
+            // IndexOf
+
+            // Insert
+
+            // Remove
+
+            // List to Array
+
+            // Array to List
+
+            ////////////////////////
+            // SORT / REVERSE
+            ////////////////////////
+
+
+
+            // QUEUE <T>
+            //
+            // Queues are a special type of data structure that follow First-In First-Out (FIFO).
+            // With Queues, we Enqueue (add) and Dequeue (remove) items.
+
+
+            /////////////////////
+            // PROCESSING ITEMS IN A QUEUE
+            /////////////////////
+
+            // Enqueue
+
+            // Loop through and Dequeue
+
+
+            // STACK <T>
+            //
+            // Stacks are another type of data structure that follow Last-In First-Out (LIFO).
+            // With Stacks, we Push (add) and Pop (remove) items. 
+
+
+            ////////////////////
+            // PUSHING ITEMS TO THE STACK
+            //////////////////// 
+
+
+
+            ////////////////////
+            // POPPING THE STACK
+            ////////////////////
+
+
+            Console.ReadLine();
+        }
+    }
+}
