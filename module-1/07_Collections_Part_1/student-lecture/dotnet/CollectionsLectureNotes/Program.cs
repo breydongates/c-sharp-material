da38aa0c76976700a33e587171e547ca68f175ce
Matt Eland <matt.eland@gmail.com>
2020-09-21 17:04:06 -0400

    M1-7 Added switch statement code

--- module-1/07_Collections_Part_1/student-lecture/dotnet/CollectionsLectureNotes/Program.cs
+++ module-1/07_Collections_Part_1/student-lecture/dotnet/CollectionsLectureNotes/Program.cs
@@ -103,6 +103,55 @@ namespace CollectionsLectureNotes
             // POPPING THE STACK
             ////////////////////
 
+            ////////////////////
+            // SWITCH STATEMENTS
+            ////////////////////
+            ///
+            bool timeForSwitchStatements = false;
+
+            if (timeForSwitchStatements)
+            {
+                Console.WriteLine();
+
+                string favoriteLanguage = "C#";
+
+                switch (favoriteLanguage)
+                {
+                    case "C#":
+                        Console.WriteLine("I love C#!");
+                        break;
+
+                    case "Java":
+                        Console.WriteLine("Java is definitely a programming language");
+                        break;
+
+                    case "JavaScript":
+                        Console.WriteLine("JavaScript is to Java as Carpet is to Car");
+                        break;
+
+                    default:
+                        Console.WriteLine($"I don't have much to say about {favoriteLanguage}");
+                        break;
+                }
+
+                // This is equivalent to the code below:
+                if (favoriteLanguage == "C#")
+                {
+                    Console.WriteLine("I love C#!");
+                }
+                else if (favoriteLanguage == "Java")
+                {
+                    Console.WriteLine("Java is definitely a programming language");
+                }
+                else if (favoriteLanguage == "JavaScript")
+                {
+                    Console.WriteLine("JavaScript is to Java as Carpet is to Car");
+                }
+                else
+                {
+                    Console.WriteLine($"I don't have much to say about {favoriteLanguage}");
+                }
+            }
 
             Console.ReadLine();
         }
