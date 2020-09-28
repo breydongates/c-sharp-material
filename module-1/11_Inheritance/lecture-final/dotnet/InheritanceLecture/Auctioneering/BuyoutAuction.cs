9188c50a328355ca4d53a4eae0bfa12ad145ede1
Matt Eland <matt.eland@gmail.com>
2020-09-28 13:33:35 -0400

    M1-11 Lecture Final

---
+++ module-1/11_Inheritance/lecture-final/dotnet/InheritanceLecture/Auctioneering/BuyoutAuction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceLecture.Auctioneering
+{
+    /// <summary>
+    /// This is an auction that can suddenly end when a BuyoutAmount is reached
+    /// </summary>
+    public class BuyoutAuction : Auction
+    {
+        public BuyoutAuction(string item) : base(item)
+        {
+
+        }
+
+        public int BuyoutAmount { get; set; } = 100000;
+
+        public override bool PlaceBid(Bid offeredBid)
+        {
+            bool isHighBid = base.PlaceBid(offeredBid);
+
+            // If the amount >= Buyout, end the auction
+            if (offeredBid.BidAmount >= this.BuyoutAmount)
+            {
+                this.EndAuction();
+            }
+
+            return isHighBid;
+        }
+
+    }
+}
---
+++ module-1/11_Inheritance/lecture-final/dotnet/InheritanceLecture/Program.cs
@@ -0,0 +1,48 @@
+﻿using InheritanceLecture.Auctioneering;
+using System;
+
+namespace InheritanceLecture
+{
+    class Program : object
+    {
+        static void Main(string[] args)
+        {
+            // Create a new general auction
+            Console.WriteLine("Starting a general auction");
+            Console.WriteLine("-----------------");
+
+            Auction generalAuction = new Auction("Up");
+            generalAuction.Auctioneer = "Hulk Hogan";
+
+            Bid bid = new Bid("Matt", 1);
+            generalAuction.PlaceBid(bid);
+
+            generalAuction.PlaceBid(new Bid("John", 23));
+            generalAuction.PlaceBid(new Bid("Rick Astley", 13));
+
+            Console.WriteLine("Today's auction conducted by " + generalAuction.Auctioneer);
+
+            //Bid sneakyBid = new Bid("Matt", -5000);
+            //generalAuction.AllBids.Add(sneakyBid);
+
+            //....
+            // This might go on until the auction runs out of time or hits a max # of bids
+
+            // Add a reserve auction where bids don't count if they're below a threshhold
+            ReserveAuction reserveAuction = new ReserveAuction(150, "John's Car");
+            reserveAuction.PlaceBid(new Bid("Matt", 50));
+            reserveAuction.PlaceBid(new Bid("Katie", 50000));
+
+            // TODO: Add a buyout auction where a high bid can win the auction outright
+            BuyoutAuction buyAuction = new BuyoutAuction("Kryptonite");
+            buyAuction.PlaceBid(new Bid("Bruce Wayne", 42));
+            buyAuction.PlaceBid(new Bid("Clark Kent", 250));
+            buyAuction.PlaceBid(new Bid("Bruce Wayne", 1000000));
+            buyAuction.PlaceBid(new Bid("Clark Kent", 1000001));
+
+            // buyAuction.EndAuction();
+
+            Console.ReadLine();
+        }
+    }
+}
