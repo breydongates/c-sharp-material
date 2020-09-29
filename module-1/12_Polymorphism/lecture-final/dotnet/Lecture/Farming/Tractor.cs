dbcd92cad6bc8924410e46814503e543915b2b19
Matt Eland <matt.eland@gmail.com>
2020-09-29 13:01:24 -0400

    M1-12 Lecture Final

---
+++ module-1/12_Polymorphism/lecture-final/dotnet/Lecture/Farming/Tractor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Farming
+{
+    public class Tractor : IMakesNoise, IRusts
+    {
+        public void PlowField()
+        {
+
+        }
+
+        public void Rust(int numTimes)
+        {
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Tractorzilla";
+            }
+        }
+
+        public string MakeSoundOnce()
+        {
+            return "vvvvvvvvvvvvvvvroooooom";
+        }
+
+        public string MakeSoundTwice()
+        {
+            return "VRRRRRROOOOOOOOOOOOM";
+        }
+    }
+}
