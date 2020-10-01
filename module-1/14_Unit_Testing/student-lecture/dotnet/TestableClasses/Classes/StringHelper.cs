b64d749e557c07ccf2a2de79c916c7a92ff48081
Matt Eland <matt.eland@gmail.com>
2020-09-30 22:30:53 -0400

    M1-14 Unit test lecture & exercises

---
+++ module-1/14_Unit_Testing/student-lecture/dotnet/TestableClasses/Classes/StringHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestableClasses.Classes
+{
+    public class StringHelper
+    {
+        public string MakeAbba(string a, string b)
+        {
+            return a + b + b + a;
+        }
+
+        public string GetFirstString(string[] strings)
+        {
+            return strings[0];
+        }
+
+        public string[] ConvertToUpperCase(string[] otherStrings)
+        {
+            string[] output = new string[otherStrings.Length];
+
+            for (int i = 0; i < otherStrings.Length; i++)
+            {
+                output[i] = otherStrings[i].ToUpper();
+            }
+
+            return output;
+        }
+
+    }
+}
---
+++ module-1/14_Unit_Testing/student-lecture/dotnet/TestableClassesTests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestableClassesTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void CalculatorShouldAddTwoAndTwoCorrectly()
+        {
+            // Arrange  - Set up the code we want to test
+
+            // Act      - Do some form of an action that we want to verify is correct
+
+            // Assert   - Verify that the system under test behaved the way we wanted it to
+        }
+    }
+}
