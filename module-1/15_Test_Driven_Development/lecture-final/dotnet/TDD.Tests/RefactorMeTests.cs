b3bab16019ed700a15163fe0c5b90de5bb6a9f65
Matt Eland <matt.eland@gmail.com>
2020-10-02 15:10:30 -0400

    M1-15 Lecture-final

---
+++ module-1/15_Test_Driven_Development/lecture-final/dotnet/TDD.Tests/RefactorMeTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD.Tests
+{
+    [TestClass]
+    public class RefactorMeTests
+    {
+        [TestMethod]
+        [DataRow("Chocolate", 2, "ChoCho")]
+        [DataRow("Chocolate", 3, "ChoChoCho")]
+        [DataRow("Chocolate", -5, "")]
+        [DataRow("Abc", 3, "AbcAbcAbc")]
+        [DataRow("Ab", 3, "AbAbAb")]
+        [DataRow("A", 4, "AAAA")]
+        [DataRow("", 4, "")]
+        [DataRow("Abc", 0, "")]
+        [DataRow(null, 0, "")]
+        public void FrontTimes(string input, int numTimes, string expected)
+        {
+            // Arrange
+            RefactorMe exercises = new RefactorMe();
+
+            // Act
+            string actual = exercises.FrontTimes(input, numTimes);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ module-1/15_Test_Driven_Development/lecture-final/dotnet/TDD/RefactorMe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD
+{
+    public class RefactorMe
+    {
+        public string FrontTimes(string input, int timesToRepeat)
+        {
+            string result = "";
+
+            // Maximum number of characters to repeat
+            int maxCharacters = 3;
+
+            for (int i = 0; i < timesToRepeat; i++)
+            {
+                // We need to worry about IndexOutOfRange with strings < maxCharacters long
+                if (input.Length < maxCharacters)
+                {
+                    result += input;
+                }
+                else
+                {
+                    result += input.Substring(0, maxCharacters);
+                }
+            }
+
+            return result;
+        }
+    }
+}
