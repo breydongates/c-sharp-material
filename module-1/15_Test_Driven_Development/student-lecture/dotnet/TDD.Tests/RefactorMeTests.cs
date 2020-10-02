a090aa74a1e8b0271b03f7a4692141818ca10b6e
Matt Eland <matt.eland@gmail.com>
2020-10-01 22:43:02 -0400

    M1-15 Added tests for FrontTimes

---
+++ module-1/15_Test_Driven_Development/student-lecture/dotnet/TDD.Tests/RefactorMeTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD.Tests
+{
+    [TestClass]
+    public class RefactorMeTests
+    {
+        [TestMethod]
+        [DataRow("Chocolate", 2, "ChoCho")]
+        [DataRow("Chocolate", 3, "ChoChoCho")]
+        [DataRow("Abc", 3, "AbcAbcAbc")]
+        [DataRow("Ab", 3, "AbAbAb")]
+        [DataRow("A", 4, "AAAA")]
+        [DataRow("", 4, "")]
+        [DataRow("Abc", 0, "")]
+        public void FrontTimes(string input, int numTimes, string expected)
+        {
+            // Arrange
+            RefactorMe exercises = new RefactorMe();
+
+            // Act
+            string actual = exercises.FrontTimes(input, numTimes);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
