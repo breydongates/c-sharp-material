e216b9fb06e9354a4d2c8e3c12d9d42da62a25f9
Matt Eland <matt.eland@gmail.com>
2020-10-01 23:00:17 -0400

    M1-15 Added TODOs in lecture code

--- module-1/15_Test_Driven_Development/student-lecture/dotnet/TDD.Tests/PayrollSystemTests.cs
+++ module-1/15_Test_Driven_Development/student-lecture/dotnet/TDD.Tests/PayrollSystemTests.cs
@@ -8,6 +8,19 @@ namespace TDD.Tests
     [TestClass]
     public class PayrollSystemTests
     {
-        // So much testing goodness will happen here!
+        // Our requirements:
+
+        // 0. We can create a payroll system
+
+        // 1. HireEmployee should accept a name and salary and return true
+
+        // 2. A TotalPayroll property which gives us the current total pay
+
+        // 3. FireEmployee should accept a name and remove that employee from the organization(TotalPayroll should no longer include them)
+
+        // 4. Employees property should return the names of all current employees
+
+        // 5. GiveRaise should increase all current employees salaries by a specific %
+
     }
 }
