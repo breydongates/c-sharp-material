3e528a28728df1a4d093650fcfc4b097989c045b
Matt Eland <matt.eland@gmail.com>
2020-10-01 22:33:23 -0400

    M1-15 Added refactoring exercise

---
+++ module-1/15_Test_Driven_Development/student-lecture/dotnet/TDD.Tests/PayrollSystemTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD.Tests
+{
+    [TestClass]
+    public class PayrollSystemTests
+    {
+        // So much testing goodness will happen here!
+    }
+}
---
+++ module-1/15_Test_Driven_Development/student-lecture/dotnet/TDD/RefactorMe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD
+{
+    public class RefactorMe
+    {
+        public string FrontTimes(string str, int n)
+        {
+            string myEpicString = "";
+
+            if (str.Length <= 3)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    myEpicString += str;
+                }
+                return myEpicString;
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    myEpicString += str.Substring(0, 3);
+                }
+                return myEpicString;
+            }
+        }
+    }
+}
