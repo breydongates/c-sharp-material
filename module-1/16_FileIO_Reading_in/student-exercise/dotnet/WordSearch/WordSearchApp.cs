9db32ab5134aa46e36a618798cae3b329b041468
Matt Eland <matt.eland@gmail.com>
2020-10-04 15:19:49 -0400

    M1-16 Student Exercise & Lecture Code

---
+++ module-1/16_FileIO_Reading_in/student-exercise/dotnet/WordSearch/WordSearchApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearch
+{
+    public class WordSearchApp
+    {
+        public void Run()
+        {
+            //1. Ask the user for the search string
+            //2. Ask the user for the file path
+
+            //3. Open the file
+
+            //4. Loop through each line in the file
+            //5. If the line contains the search string, print it out along with its line number
+
+            // See Part 2 in the Readme.md file for additional changes
+        }
+    }
+}
