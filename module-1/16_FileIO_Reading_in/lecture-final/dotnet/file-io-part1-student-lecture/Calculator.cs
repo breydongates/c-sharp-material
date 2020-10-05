6b77ea4d2f88fb243bb808252b75438c0b0c5d0d
Matt Eland <matt.eland@gmail.com>
2020-10-05 13:51:48 -0400

    M1-16 Lecture-final

---
+++ module-1/16_FileIO_Reading_in/lecture-final/dotnet/file-io-part1-student-lecture/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileInputLecture
+{
+    /// <summary>
+    /// This class is responsible for generating division by zero exceptions for lecture purposes
+    /// </summary>
+    public class Calculator
+    {
+        public int Divide(int x, int y)
+        {
+            // TODO: Throw an argument exception here instead of allowing 0 for y
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException("y", "Dude, you can't divide by zero!");
+            }
+
+            return x / y;
+        }
+    }
+}
---
+++ module-1/16_FileIO_Reading_in/lecture-final/dotnet/file-io-part1-student-lecture/CensoredFileReader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileInputLecture
+{
+    /// <summary>
+    /// This class is responsible for reading data in from a specified file and printing out the text
+    /// with a specific word redacted.
+    /// </summary>
+    public class CensoredFileReader
+    {
+        public CensoredFileReader(string wordToCensor, string censoredText)
+        {
+            this.WordToCensor = wordToCensor;
+            this.CensoredText = censoredText;
+        }
+
+        public string WordToCensor { get; set; }
+        public string CensoredText { get; set; }
+
+        public void CensorFile(string filePath)
+        {
+            // For practice, it might be nice to validate filepath
+
+            Console.WriteLine("Censoring the contents of " + filePath);
+
+            try
+            {
+                // Add a using statement that creates a StreamReader pointing at the correct file
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    // While we haven't reached the end of the file...
+                    while (!reader.EndOfStream)
+                    {
+                        // Read in the next line from the file
+                        string line = reader.ReadLine();
+
+                        // If the line needs censoring,
+                        if (line.Contains(this.WordToCensor))
+                        {
+                            // ... Censor the line as needed by replacing WordToCensor with CensoredText
+                            line = line.Replace(this.WordToCensor, this.CensoredText);
+
+                            // ... Print the censored line to the Console
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Could not find the file " + filePath);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An error occurred trying to read from the file: " + ex.Message);
+            }
+
+            Console.WriteLine("Done Censoring");
+        }
+    }
+}
