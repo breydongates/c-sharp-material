1da392c7d28a8c5601ae3e034496a287a274ab8d
Matt Eland <matt.eland@gmail.com>
2020-10-06 12:26:03 -0400

    M1-17 - Corrections to homework readme, added a GetReadmeDir method

--- module-1/17_FileIO_Writing_out/student-exercise/dotnet/FizzWriter/Program.cs
+++ module-1/17_FileIO_Writing_out/student-exercise/dotnet/FizzWriter/Program.cs
@@ -8,5 +8,21 @@ namespace FizzWriter
         {
             Console.WriteLine("Hello World!");
         }
+
+        /// <summary>
+        /// Gets the full path to the directory with the readme file
+        /// </summary>
+        /// <returns>The full path to the directory with the readme file</returns>
+        public static string GetReadmeDir()
+        {
+            string dir = Environment.CurrentDirectory;
+
+            System.IO.DirectoryInfo netcoreappDir = System.IO.Directory.GetParent(dir);
+            System.IO.DirectoryInfo binDir = System.IO.Directory.GetParent(netcoreappDir.FullName);
+            System.IO.DirectoryInfo codeDir = System.IO.Directory.GetParent(binDir.FullName);
+            System.IO.DirectoryInfo readmeDir = System.IO.Directory.GetParent(codeDir.FullName);
+
+            return readmeDir.FullName;
+        }
     }
 }
