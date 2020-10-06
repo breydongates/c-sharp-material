b80d25469a14b65823f8e5f40f2770a3c9633da1
Matt Eland <matt.eland@gmail.com>
2020-10-05 22:27:37 -0400

    M1-17 Student Lecture

---
+++ module-1/17_FileIO_Writing_out/student-lecture/dotnet/file-io-part1-student-lecture/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FileInputLecture
+{
+    class Program
+    {
+        static void Main(string[] args) // What can we do with these args?
+        {
+            // It'd be nice if we didn't have to escape this string...
+            string myPath = "C:\\Users\\Student";
+            
+            string sourceFile = Path.Combine(myPath, "alice.txt");
+            string destFile = Path.Combine(myPath, "redacted.txt");
+
+            // Create a redacted copy of the modified lines from source file in redacted.txt
+            CensoredFileReader censor = new CensoredFileReader("Alice", "[REDACTED]");
+            censor.CensorFile(sourceFile, destFile);
+        }
+    }
+}
