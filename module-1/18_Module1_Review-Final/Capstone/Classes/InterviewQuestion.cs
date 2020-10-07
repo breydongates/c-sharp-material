cb9daabed71cf62c1e76f93e494e95c3445ff181
Matt Eland <matt.eland@gmail.com>
2020-10-07 13:43:42 -0400

    M1-18 Final Review Day Code

---
+++ module-1/18_Module1_Review-Final/Capstone/Classes/InterviewQuestion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    public class InterviewQuestion
+    {
+        public InterviewQuestion(string name, int id)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+
+        // This class should contain the definition for each piece of data in the CSV: ID and Text
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return this.Id + ") " + this.Name;
+        }
+    }
+}
---
+++ module-1/18_Module1_Review-Final/Capstone/Classes/QuestionManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    /// <summary>
+    /// This class is responsible for holding interview questions and providing random questions to users
+    /// </summary>
+    public class QuestionManager
+    {
+        public QuestionManager()
+        {
+            // this.items.Add(new InterviewQuestion("What’s the difference between C# and .NET?", 1));
+        }
+
+        private List<InterviewQuestion> items = new List<InterviewQuestion>();
+
+        public List<InterviewQuestion> AllQuestions
+        {
+            get
+            {
+                return this.items;
+            }
+        }
+        // TODO: This class should provide a way to add an interview question
+
+        public void Add(InterviewQuestion question)
+        {
+            this.items.Add(question);
+        }
+
+        public int NumCorrect { get; set; } = 8;
+        public int NumGuessed { get; set; } = 10;
+
+        // TODO: This class should provide a way to get all interview questions
+        
+        // TODO: This class should provide a way to get a random interview question
+    }
+}
