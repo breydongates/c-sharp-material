44d07af534ed0f40fa6ca382c10e589d8f078186
Breydon <breydonagates@gmail.com>
2020-10-13 09:16:35 -0400

    mod 2 tuesday

--- module-2/01_Introduction_to_Databases/student-lecture/lecture-queries.sql
+++ module-2/01_Introduction_to_Databases/student-lecture/lecture-queries.sql
@@ -1,37 +1,43 @@
 -- SELECT ... FROM
 -- Selecting the names for all countries
-
+   SELECT * FROM country; --selects all from country
+   SELECT name FROM country; -- selects all names from countries
 
 -- Selecting the name and population of all countries
-
+SELECT name, population FROM country;
 
 -- Selecting all columns from the city table
-
+SELECT * FROM city;
 
 -- SELECT ... FROM ... WHERE
 -- Selecting the cities in Ohio
-
+SELECT * FROM city WHERE district = 'Ohio';
 
 -- Selecting countries that gained independence in the year 1776
-
+SELECT * FROM country WHERE indepyear = 1776;
 
 -- Selecting countries not in Asia
-
+SELECT * FROM country WHERE continent != 'Asia';
 
 -- Selecting countries that do not have an independence year
+SELECT * FROM country WHERE indepyear IS NULL;
 
 -- Selecting countries that do have an independence year
-
+SELECT * FROM country WHERE indepyear IS NOT NULL;
 
 -- Selecting countries that have a population greater than 5 million
-
+SELECT * FROM country WHERE population > 5000000;
 
 
 -- SELECT ... FROM ... WHERE ... AND/OR
 -- Selecting cities in Ohio and Population greater than 400,000
+SELECT * FROM city WHERE district = 'Ohio' AND population > 400000;
 
 -- Selecting country names on the continent North America or South America
-
+SELECT name AS Name, continent AS continent 
+FROM country
+WHERE continent = 'North America'
+OR continent = 'South America'
 
 
 
@@ -39,5 +45,7 @@
 -- Selecting the population, life expectancy, and population per area
 --	note the use of the 'as' keyword
 
+SELECT surfacearea, population, population/surfacearea AS 'Population Density'
+FROM country
 
 
