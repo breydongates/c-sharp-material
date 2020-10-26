582475c557f9117d39b56c92f0a4c24a03f6025c
Matt Eland <matt.eland@gmail.com>
2020-10-26 13:44:05 -0400

    M3-2 Tutorials, Lecture, and Exercises

---
+++ module-3/02-HTTP-Web-Services-POST/student-lecture/dotnet/HotelApp/APIService.cs
@@ -0,0 +1,51 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace HTTP_Web_Services_POST_PUT_DELETE_lecture
+{
+    class APIService
+    {
+        private readonly string API_URL = "";
+        private readonly RestClient client = new RestClient();
+
+        public APIService(string api_url)
+        {
+            if (api_url.Contains("REPLACEME"))
+            {
+                throw new ArgumentOutOfRangeException(nameof(api_url), "You didn't set your laptop ID in UserInterface.cs");
+            }
+            API_URL = api_url;
+        }
+
+        public List<Hotel> GetHotels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> GetReservations(int hotelId = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation GetReservation(int reservationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation AddReservation(Reservation newReservation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation UpdateReservation(Reservation reservationToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteReservation(int reservationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
