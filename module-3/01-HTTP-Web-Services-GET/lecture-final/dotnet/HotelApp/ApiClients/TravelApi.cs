bd258eb5c248482bcc0592ce926f0082f4e6cbc0
Matt Eland <matt.eland@gmail.com>
2020-10-26 13:43:12 -0400

    M3-1 Lecture Final

---
+++ module-3/01-HTTP-Web-Services-GET/lecture-final/dotnet/HotelApp/ApiClients/TravelApi.cs
@@ -0,0 +1,23 @@
+﻿using HTTP_Web_Services_GET_lecture.Data;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTTP_Web_Services_GET_lecture.ApiClients
+{
+    public class TravelApi
+    {
+        public string GetRecommendedDestination()
+        {
+            // TODO: Call out to http://swapi.dev/api/planets/5/
+            RestClient client = new RestClient();
+            RestRequest request = new RestRequest("https://swapi.dev/api/planets/2/");
+            IRestResponse<Planet> result = client.Get<Planet>(request);
+
+            Planet destination = result.Data;
+
+            return destination.Name;
+        }
+    }
+}
