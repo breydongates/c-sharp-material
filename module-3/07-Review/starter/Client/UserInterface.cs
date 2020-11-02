e79706a1ddd6b0da308624abb768adaf67459340
Matt Eland <matt.eland@gmail.com>
2020-11-02 16:56:38 -0500

    M3-7 Early version of review project

---
+++ module-3/07-Review/starter/Client/UserInterface.cs
@@ -0,0 +1,124 @@
+﻿using SallyClient.Data;
+using System;
+using TenmoClient;
+
+namespace SallyClient
+{
+    public class UserInterface
+    {
+        private readonly ConsoleService consoleService = new ConsoleService();
+        private readonly AuthService authService = new AuthService();
+
+        private bool shouldExit = false;
+
+        public void Start()
+        {
+            while (!shouldExit)
+            {
+                while (!authService.IsLoggedIn)
+                {
+                    ShowLogInMenu();
+                }
+
+                // If we got here, then the user is logged in. Go ahead and show the main menu
+                ShowMainMenu();
+            }
+        }
+
+        private void ShowLogInMenu()
+        {
+            Console.WriteLine("Welcome to Sally the Squirrel's One-Stop Interview Question Shop!");
+            Console.WriteLine("1: Login");
+            Console.WriteLine("2: Register");
+            Console.Write("Please choose an option: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int loginRegister))
+            {
+                Console.WriteLine("Invalid input. Please enter only a number.");
+            }
+            else if (loginRegister == 1)
+            {
+                HandleUserLogin();
+            }
+            else if (loginRegister == 2)
+            {
+                HandleUserRegister();
+            }
+            else
+            {
+                Console.WriteLine("Invalid selection.");
+            }
+        }
+
+        private void ShowMainMenu()
+        {
+            int menuSelection = -1;
+            while (menuSelection != 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Welcome to Sally's! Please make a selection: ");
+                Console.WriteLine("1: Get a random question");
+                Console.WriteLine("2: Create a new question");
+                Console.WriteLine("3: Log in as different user");
+                Console.WriteLine("0: Exit");
+                Console.WriteLine("---------");
+                Console.Write("Please choose an option: ");
+
+                if (!int.TryParse(Console.ReadLine(), out menuSelection))
+                {
+                    Console.WriteLine("Invalid input. Please enter only a number.");
+                }
+                else
+                {
+                    switch (menuSelection)
+                    {
+                        case 1: // Get a random question
+                            Console.WriteLine("NOT IMPLEMENTED!"); // TODO: Implement me
+                            break;
+                        case 2: // Create a new question
+                            Console.WriteLine("NOT IMPLEMENTED!"); // TODO: Implement me
+                            break;
+                        case 3: // Log out
+                            Console.WriteLine();
+                            UserService.SetLogin(new API_User()); //wipe out previous login info
+                            return;
+                        case 0:
+                            Console.WriteLine("Goodbye!");
+                            shouldExit = true;
+                            return;
+                        default:
+                            Console.WriteLine("That's not a valid choice. Try again.");
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void HandleUserRegister()
+        {
+            bool isRegistered = false;
+
+            while (!isRegistered) //will keep looping until user is registered
+            {
+                LoginUser registerUser = consoleService.PromptForLogin();
+                isRegistered = authService.Register(registerUser);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Registration successful. You can now log in.");
+        }
+
+        private void HandleUserLogin()
+        {
+            while (!UserService.IsLoggedIn()) //will keep looping until user is logged in
+            {
+                LoginUser loginUser = consoleService.PromptForLogin();
+                API_User user = authService.Login(loginUser);
+                if (user != null)
+                {
+                    UserService.SetLogin(user);
+                }
+            }
+        }
+    }
+}
