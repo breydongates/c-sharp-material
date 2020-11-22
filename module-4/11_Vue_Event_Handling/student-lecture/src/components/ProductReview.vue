34f2403ab7c6bd85ea33d484c00b0145af419244
Matt Eland <matt.eland@gmail.com>
2020-11-22 15:40:20 -0500

    M4-11 Switched to Semantic HTML

--- module-4/11_Vue_Event_Handling/student-lecture/src/components/ProductReview.vue
+++ module-4/11_Vue_Event_Handling/student-lecture/src/components/ProductReview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main">
+  <main>
     <h2>Product Reviews for {{ name }}</h2>
 
     <p class="description">{{ description }}</p>
@@ -36,7 +36,7 @@
       </div>
     </div>
 
-    <div
+    <section
       class="review"
       v-bind:class="{ favorited: review.favorited }"
       v-for="review in reviews"
@@ -60,8 +60,8 @@
         Favorite?
         <input type="checkbox" v-model="review.favorited" />
       </p>
-    </div>
-  </div>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -146,16 +146,16 @@ export default {
 </script>
 
 <style scoped>
-div.main {
+main {
   margin: 1rem 0;
 }
 
-div.main div.well-display {
+main div.well-display {
   display: flex;
   justify-content: space-around;
 }
 
-div.main div.well-display div.well {
+main div.well-display div.well {
   display: inline-block;
   width: 15%;
   border: 1px black solid;
@@ -164,43 +164,43 @@ div.main div.well-display div.well {
   margin: 0.25rem;
 }
 
-div.main div.well-display div.well span.amount {
+main div.well-display div.well span.amount {
   color: darkslategray;
   display: block;
   font-size: 2.5rem;
 }
 
-div.main div.review {
+main section.review {
   border: 1px black solid;
   border-radius: 6px;
   padding: 1rem;
   margin: 10px;
 }
 
-div.main div.review.favorited {
+main section.review.favorited {
   background-color: lightyellow;
 }
 
-div.main div.review div.rating {
+main section.review div.rating {
   height: 2rem;
   display: inline-block;
   vertical-align: top;
   margin: 0 0.5rem;
 }
 
-div.main div.review div.rating img {
+main section.review div.rating img {
   height: 100%;
 }
 
-div.main div.review p {
+main section.review p {
   margin: 20px;
 }
 
-div.main div.review h3 {
+main section.review h3 {
   display: inline-block;
 }
 
-div.main div.review h4 {
+main section.review h4 {
   font-size: 1rem;
 }
 
