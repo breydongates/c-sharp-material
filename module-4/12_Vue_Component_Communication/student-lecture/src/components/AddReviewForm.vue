d27ff4e62052b3a34f50b63557a12f3eda6f327d
Matt Eland <matt.eland@gmail.com>
2020-11-23 23:32:52 -0500

    M4-12 Good code for tomorrow

---
+++ module-4/12_Vue_Component_Communication/student-lecture/src/components/AddReviewForm.vue
@@ -0,0 +1,80 @@
+<template>
+  <section>
+    <!-- Show the addReviewSection when clicked and hide this button -->
+    <input
+      type="button"
+      value="Add Review"
+      v-on:click="showAddReview = true"
+      v-if="!showAddReview"
+    />
+
+    <!-- Allow the user to add a review -->
+    <form v-if="showAddReview">
+      <div class="form-element">
+        <label for="reviewer">Name:</label>
+        <input id="reviewer" type="text" v-model.trim="newReview.reviewer" />
+      </div>
+      <div class="form-element">
+        <label for="title">Title:</label>
+        <input id="title" type="text" v-model.trim="newReview.title" />
+      </div>
+      <div class="form-element">
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="newReview.rating">
+          <option value="1">1 Star</option>
+          <option value="2">2 Stars</option>
+          <option value="3">3 Stars</option>
+          <option value="4">4 Stars</option>
+          <option value="5">5 Stars</option>
+        </select>
+      </div>
+      <div class="form-element">
+        <label for="review">Review:</label>
+        <textarea id="review" v-model.trim="newReview.review"></textarea>
+      </div>
+      <input
+        type="submit"
+        value="Save"
+        v-on:click.prevent="addNewReviewRevised()"
+      />
+      <input type="button" value="Cancel" v-on:click="showAddReview = false" />
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AddReviewForm",
+  data() {
+    return {
+      newReview: {
+        reviewer: "",
+        title: "",
+        review: "",
+        rating: 0,
+        favorited: false,
+      },
+      showAddReview: false,
+    };
+  },
+  methods: {
+    addNewReviewRevised() {
+      // TODO: Add this.newReview to a centralized object store
+
+      // Reset the new review so it's a new object with blank values
+      this.newReview = {
+        reviewer: "",
+        title: "",
+        rating: 0,
+        favorited: false,
+        review: "",
+      };
+
+      this.showAddReview = false;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ module-4/12_Vue_Component_Communication/student-lecture/src/components/ReviewStatistics.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="well-display">
+    <div class="well" v-on:click="setFilter(0)">
+      <span class="amount">{{ averageRating }}</span>
+      Average Rating
+    </div>
+
+    <div class="well" v-on:click="setFilter(1)">
+      <span class="amount">{{ numberOfOneStarReviews }}</span>
+      1 Star Review{{ numberOfOneStarReviews === 1 ? "" : "s" }}
+    </div>
+
+    <div class="well" v-on:click="setFilter(2)">
+      <span class="amount">{{ numberOfTwoStarReviews }}</span>
+      2 Star Review{{ numberOfTwoStarReviews === 1 ? "" : "s" }}
+    </div>
+
+    <div class="well" v-on:click="setFilter(3)">
+      <span class="amount">{{ numberOfThreeStarReviews }}</span>
+      3 Star Review{{ numberOfThreeStarReviews === 1 ? "" : "s" }}
+    </div>
+
+    <div class="well" v-on:click="setFilter(4)">
+      <span class="amount">{{ numberOfFourStarReviews }}</span>
+      4 Star Review{{ numberOfFourStarReviews === 1 ? "" : "s" }}
+    </div>
+
+    <div class="well" v-on:click="setFilter(5)">
+      <span class="amount">{{ numberOfFiveStarReviews }}</span>
+      5 Star Review{{ numberOfFiveStarReviews === 1 ? "" : "s" }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReviewStatistics",
+  computed: {
+    reviews() {
+      return []; // TODO: Get this data from a centralized store
+    },
+    averageRating() {
+      let sum = this.reviews.reduce((currentSum, review) => {
+        return currentSum + review.rating;
+      }, 0);
+      return (sum / this.reviews.length).toFixed(2);
+    },
+    numberOfOneStarReviews() {
+      return this.reviews.reduce((currentCount, review) => {
+        return currentCount + (review.rating === 1);
+      }, 0);
+    },
+    numberOfTwoStarReviews() {
+      return this.reviews.reduce((currentCount, review) => {
+        return currentCount + (review.rating === 2);
+      }, 0);
+    },
+    numberOfThreeStarReviews() {
+      return this.reviews.reduce((currentCount, review) => {
+        return currentCount + (review.rating === 3);
+      }, 0);
+    },
+    numberOfFourStarReviews() {
+      return this.reviews.reduce((currentCount, review) => {
+        return currentCount + (review.rating === 4);
+      }, 0);
+    },
+    numberOfFiveStarReviews() {
+      return this.reviews.reduce((currentCount, review) => {
+        return currentCount + (review.rating === 5);
+      }, 0);
+    },
+  },
+  methods: {
+    setFilter(level) {
+      console.debug("Set filter level to " + level);
+      // TODO: Set a global state's filter by review setting
+    },
+  },
+};
+</script>
+
+<style>
+</style>
